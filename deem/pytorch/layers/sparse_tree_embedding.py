7657e8782310c9855f419186d681475797baccae
Benyu Zhang <byzhang@gmail.com>
2021-11-27 17:37:35 -0800

    fix to_device

--- deem/pytorch/layers/sparse_tree_embedding.py
+++ deem/pytorch/layers/sparse_tree_embedding.py
@@ -9,11 +9,13 @@ class ObliviousTreeEmbedding(nn.Module):
         super().__init__()
         self.device = set_device(gpu)
         self.layers = [nn.Linear(in_features, width) for _ in range(depth)]
+        self.layers = nn.ModuleList(self.layers)
         self.entmax_alpha = entmax_alpha
         self.to(self.device)
 
     def forward(self, X):
-        factors = [entmax.entmax_bisect(layer(X), alpha=self.entmax_alpha, dim=-1, n_iter=25) for layer in self.layers]
+        dense_factors = [layer(X) for layer in self.layers]
+        factors = [entmax.entmax_bisect(factor, alpha=self.entmax_alpha, dim=-1, n_iter=25) for factor in dense_factors]
         output = factors[0]
         for x in range(1, len(factors)):
             output = torch.einsum('bi,bj->bij', (output, factors[x]))  # batch outer product
